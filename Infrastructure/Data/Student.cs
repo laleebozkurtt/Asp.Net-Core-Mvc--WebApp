a95ea144d3d7b127e5687e6fa7408a74006bf9d6
laleebozkurtt <162982040+laleebozkurtt@users.noreply.github.com>
2025-03-14 10:03:46 +0300

    Gereksiz dosyalar kaldırıldı

---
+++ Infrastructure/Data/Student.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApp.Models
+{
+    public class Student
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public DateTime BirthDate { get; set; }
+        public DateTime RegisterDate { get; set; }
+        public int? WillAttend { get; set; }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+A beginner level logging simulation was created to learn the basics of asp.net core mvc architecture.
